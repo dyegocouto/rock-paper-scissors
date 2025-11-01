cb0e2d8c6df2b160c0be2788df110af6b742adbd
dyegocouto <175648963+dyegocouto@users.noreply.github.com>
2025-11-01 11:33:19 -0300

    feat: add core logic for playing a round
    Clicking a button pitches its value against a randomly drawn value
    by the computer. Both are then printed to the console.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-const buttonsContainer = document.querySelector(".buttons-container");
+let roundWinner = "";
+
+function getComputerChoice() {
+  return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerChoice, computerChoice) {
+  if (
+    (playerChoice === "rock" && computerChoice === "paper") ||
+    (playerChoice === "paper" && computerChoice === "scissors") ||
+    (playerChoice === "scissors" && computerChoice === "rock")
+  ) {
+    roundWinner = "computer";
+  } else {
+    roundWinner = "player";
+  }
+
+  console.log("The round winner is the " + roundWinner);
+}
 
 function handleClick(e) {
   const playerChoice = e.target.closest("button").id;
-  console.log(playerChoice);
+  playRound(playerChoice, getComputerChoice());
 }
+
+const buttonsContainer = document.querySelector(".buttons-container");
 
 buttonsContainer.addEventListener("click", handleClick);
