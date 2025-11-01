67658ffe86866d799158c499b41dd6ce0d2dad51
dyegocouto <175648963+dyegocouto@users.noreply.github.com>
2025-11-01 11:34:59 -0300

    feat: add logic for keeping score
    A scoreboard now keeps track of previous winners. Also printed to
    the console.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const score = { player: 0, computer: 0 };
 let roundWinner = "";
 
 function getComputerChoice() {
@@ -10,12 +11,15 @@ function playRound(playerChoice, computerChoice) {
     (playerChoice === "paper" && computerChoice === "scissors") ||
     (playerChoice === "scissors" && computerChoice === "rock")
   ) {
+    score.computer++;
     roundWinner = "computer";
   } else {
+    score.player++;
     roundWinner = "player";
   }
 
   console.log("The round winner is the " + roundWinner);
+  console.log("Current score: " + JSON.stringify(score));
 }
 
 function handleClick(e) {
